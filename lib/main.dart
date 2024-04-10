8b8dbca5c7f75a8f9f085f611cd97eb1983c67b3
Mohamed Ali Badawy <66221310+MohamedAliBadawy@users.noreply.github.com>
2024-04-10 04:16:43 +0200

    Update lib/main.dart
    Co-authored-by: Loay Ghreeb <52158423+LoayGhreeb@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Task',
+      title: 'The Quran',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
