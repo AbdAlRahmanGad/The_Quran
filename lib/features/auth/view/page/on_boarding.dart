2c75ec1053c26e5b8b77dd614bd9acf65dd5e05b
MohamedAliBadawy <medooali6262@gmail.com>
2024-05-01 17:06:11 +0300

    fixed on boarding screen button position

--- lib/features/auth/view/page/on_boarding.dart
+++ lib/features/auth/view/page/on_boarding.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:the_quran/features/auth/view/page/login_screen.dart';
 
 class OnBoarding extends StatefulWidget {
@@ -14,7 +17,6 @@ class _OnBoardingState extends State<OnBoarding> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: PageView(
         controller: _pageController,
@@ -22,11 +24,13 @@ class _OnBoardingState extends State<OnBoarding> {
           Stack(
             children: <Widget>[
               Image.asset('assets/images/page_1.png', fit: BoxFit.cover),
-              Align(
-                alignment: Alignment.bottomCenter,
+              Positioned(
+                bottom: MediaQuery.of(context).size.height * 0.1,
+                right: MediaQuery.of(context).size.width * 0.39,
                 child: ElevatedButton(
                   style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF87D1A4)),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF87D1A4)),
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30.0),
@@ -48,11 +52,13 @@ class _OnBoardingState extends State<OnBoarding> {
           Stack(
             children: <Widget>[
               Image.asset('assets/images/page_2.png', fit: BoxFit.cover),
-              Align(
-                alignment: Alignment.bottomCenter,
+              Positioned(
+                bottom: MediaQuery.of(context).size.height * 0.1,
+                right: MediaQuery.of(context).size.width * 0.39,
                 child: ElevatedButton(
                   style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF87D1A4)),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF87D1A4)),
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30.0),
@@ -74,11 +80,13 @@ class _OnBoardingState extends State<OnBoarding> {
           Stack(
             children: <Widget>[
               Image.asset('assets/images/page_3.png', fit: BoxFit.cover),
-              Align(
-                alignment: Alignment.bottomCenter,
+              Positioned(
+                bottom: MediaQuery.of(context).size.height * 0.1,
+                right: MediaQuery.of(context).size.width * 0.39,
                 child: ElevatedButton(
                   style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF87D1A4)),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF87D1A4)),
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30.0),
@@ -89,7 +97,8 @@ class _OnBoardingState extends State<OnBoarding> {
                     // TODO go to home page
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const LoginScreen()),
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()),
                     );
                   },
                   child: const Text('Start'),
