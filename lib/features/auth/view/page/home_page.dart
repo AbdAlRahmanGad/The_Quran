2067d38456cd25bd58aa782d7890f3b7a7d45916
AbdAlRahmanGad <abdobngad@gmail.com>
2024-05-02 15:06:07 +0300

    finish the database creation, insertion, DatabaseHelper

--- lib/features/auth/view/page/home_page.dart
+++ lib/features/auth/view/page/home_page.dart
@@ -1,44 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:the_quran/features/auth/controller/bloc/auth_controller_bloc.dart';
+import 'package:the_quran/features/auth/view/page/DataBaseHelper.dart';
+import 'package:the_quran/features/auth/view/page/ProfilePage.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class Profile {
+  int id;
+  String firstName;
+  String lastName;
+  String bio;
+  String favoriteReciters;
+
+  Profile(this.id, this.firstName, this.lastName, this.bio, this.favoriteReciters);
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'bio': bio,
+      'favoriteReciters': favoriteReciters,
+    };
+    return map;
+  }
+
+  Profile.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        firstName = map['firstName'],
+        lastName = map['lastName'],
+        bio = map['bio'],
+        favoriteReciters = map['favoriteReciters'];
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _favoriteRecitersController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Profile Page'),
       ),
-      body: Center(
-        child: BlocProvider(
-          create: (context) => AuthControllerBloc(),
-          child: BlocBuilder<AuthControllerBloc, AuthControllerState>(
-            builder: (context, state) {
-              AuthControllerBloc bloc = context.read<AuthControllerBloc>();
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Welcome to the Home Page!',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      bloc.add(SignOut());
-                    },
-                    child: const Text('Log Out'),
-                  ),
-                ],
-              );
-            },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _firstNameController,
+                decoration: const InputDecoration(labelText: 'First Name'),
+              ),
+              TextFormField(
+                controller: _lastNameController,
+                decoration: const InputDecoration(labelText: 'Last Name'),
+              ),
+              TextFormField(
+                controller: _bioController,
+                decoration: const InputDecoration(labelText: 'Bio'),
+              ),
+              TextFormField(
+                controller: _favoriteRecitersController,
+                decoration: const InputDecoration(labelText: 'Favorite Reciters'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  var user = Profile(
+                    1,
+                    _firstNameController.text,
+                    _lastNameController.text,
+                    _bioController.text,
+                    _favoriteRecitersController.text,
+                  );
+                  DatabaseHelper.instance.insert(user.toMap());
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ProfilePage()),
+                  );
+                },
+                child: const Text('Create Profile'),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _bioController.dispose();
+    _favoriteRecitersController.dispose();
+    super.dispose();
   }
 }
