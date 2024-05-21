85aa51d88574554b22eeb9ec62ee995714267de4
MohamedAliBadawy <medooali6262@gmail.com>
2024-05-22 00:57:20 +0300

    Updated auth auth services

--- lib/features/auth/view/page/forgot_password_screen.dart
+++ lib/features/auth/view/page/forgot_password_screen.dart
@@ -3,8 +3,11 @@ import 'dart:developer';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:the_quran/core/utils/consts.dart';
 import 'package:the_quran/core/utils/validation.dart';
 import 'package:the_quran/features/auth/controller/bloc/auth_controller_bloc.dart';
+import 'package:the_quran/features/auth/view/components/app_text_form_field.dart';
+import 'package:the_quran/features/auth/view/components/gradient_background.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
   const ForgotPasswordScreen({super.key});
@@ -12,71 +15,86 @@ class ForgotPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Forgot Password'),
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(
+          color: Colors.white, //change your color here
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: BlocProvider(
-          create: (context) => AuthControllerBloc(),
-          child: BlocBuilder<AuthControllerBloc, AuthControllerState>(
-            builder: (context, state) {
-              if (state is AuthControllerLoadingState &&
-                  state.isLoading == true) {
-                log("loading");
-                return const Center(child: CircularProgressIndicator());
-              } else if (state is AuthControllerFeedbackState) {
-                log("success");
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  AwesomeDialog(
-                    context: context,
-                    dialogType: DialogType.success,
-                    animType: AnimType.rightSlide,
-                    title: 'Error',
-                    desc: state.feedbackMessage,
-                    btnCancelOnPress: () {},
-                    btnOkOnPress: () {},
-                  ).show();
-                });
-              } else if (state is AuthControllerFailureState) {
-                log("Failed");
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  AwesomeDialog(
-                    context: context,
-                    dialogType: DialogType.error,
-                    animType: AnimType.rightSlide,
-                    title: 'Error',
-                    desc: state.errorMessage,
-                    btnCancelOnPress: () {},
-                    btnOkOnPress: () {},
-                  ).show();
-                });
-              }
-              AuthControllerBloc bloc = context.read<AuthControllerBloc>();
-              return Column(
-                children: [
-                  Form(
-                    key: bloc.formKey,
-                    child: TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: bloc.emailController,
-                      decoration: const InputDecoration(labelText: 'Email'),
-                      validator: Validator().validateMail,
+      body: BlocProvider(
+        create: (context) => AuthControllerBloc(),
+        child: BlocBuilder<AuthControllerBloc, AuthControllerState>(
+          builder: (context, state) {
+            if (state is AuthControllerLoadingState &&
+                state.isLoading == true) {
+              log("loading");
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is AuthControllerFeedbackState) {
+              log("success");
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                AwesomeDialog(
+                  context: context,
+                  dialogType: DialogType.success,
+                  animType: AnimType.rightSlide,
+                  title: 'Error',
+                  desc: state.feedbackMessage,
+                  btnCancelOnPress: () {},
+                  btnOkOnPress: () {},
+                ).show();
+              });
+            } else if (state is AuthControllerFailureState) {
+              log("Failed");
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                AwesomeDialog(
+                  context: context,
+                  dialogType: DialogType.error,
+                  animType: AnimType.rightSlide,
+                  title: 'Error',
+                  desc: state.errorMessage,
+                  btnCancelOnPress: () {},
+                  btnOkOnPress: () {},
+                ).show();
+              });
+            }
+            AuthControllerBloc bloc = context.read<AuthControllerBloc>();
+            return ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                const GradientBackground(
+                  children: [
+                    Text(
+                      "Forgot Password",
+                      style: Consts.titleLarge,
                     ),
+                    SizedBox(height: 6),
+                    Text("Forgot Password", style: Consts.bodySmall),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Form(
+                  key: bloc.formKey,
+                  child: AppTextFormField(
+                    controller: bloc.emailController,
+                    labelText: "Email",
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: Validator().validateMail,
                   ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () async {
-                      if (bloc.emailController.text.isNotEmpty) {
-                        bloc.add(ForgetPass(bloc.emailController.text.trim()));
-                      }
-                    },
-                    child: const Text('Reset Password'),
-                  ),
-                ],
-              );
-            },
-          ),
+                ),
+                const SizedBox(height: 20),
+                FilledButton(
+                  onPressed: () async {
+                    if (bloc.emailController.text.isNotEmpty) {
+                      bloc.add(ForgetPass(bloc.emailController.text.trim()));
+                    }
+                  },
+                  child: const Text('Reset Password'),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
--- lib/features/auth/view/page/sign_up_screen.dart
+++ lib/features/auth/view/page/sign_up_screen.dart
@@ -3,10 +3,12 @@ import 'dart:developer';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:the_quran/core/utils/consts.dart';
 import 'package:the_quran/core/utils/validation.dart';
 import 'package:the_quran/features/auth/controller/bloc/auth_controller_bloc.dart';
-
-import 'home_page.dart';
+import 'package:the_quran/features/auth/view/components/app_text_form_field.dart';
+import 'package:the_quran/features/auth/view/components/gradient_background.dart';
+import 'package:the_quran/features/dashboard/view/page/dashboard_page.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({
@@ -16,86 +18,142 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Sign Up'),
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(
+          color: Colors.white, //change your color here
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: BlocProvider(
-          create: (context) => AuthControllerBloc(
-            onSignSuccess: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
-              );
-            },
-          ),
-          child: BlocBuilder<AuthControllerBloc, AuthControllerState>(
-            builder: (context, state) {
-              if (state is AuthControllerLoadingState &&
-                  state.isLoading == true) {
-                log("loading");
-                return const Center(child: CircularProgressIndicator());
-              } else if (state is AuthControllerFeedbackState) {
-                log("success");
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  AwesomeDialog(
-                    context: context,
-                    dialogType: DialogType.success,
-                    animType: AnimType.rightSlide,
-                    title: 'Error',
-                    desc: state.feedbackMessage,
-                    btnCancelOnPress: () {},
-                    btnOkOnPress: () {},
-                  ).show();
-                });
-              } else if (state is AuthControllerFailureState) {
-                log("Failed");
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  AwesomeDialog(
-                    context: context,
-                    dialogType: DialogType.error,
-                    animType: AnimType.rightSlide,
-                    title: 'Error',
-                    desc: state.errorMessage,
-                    btnCancelOnPress: () {},
-                    btnOkOnPress: () {},
-                  ).show();
-                });
-              }
-              AuthControllerBloc bloc = context.read<AuthControllerBloc>();
-              return Form(
-                key: bloc.formKey,
-                child: Column(
+      body: BlocProvider(
+        create: (context) => AuthControllerBloc(
+          onSignSuccess: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const DashboardPage()),
+            );
+          },
+        ),
+        child: BlocBuilder<AuthControllerBloc, AuthControllerState>(
+          builder: (context, state) {
+            if (state is AuthControllerLoadingState &&
+                state.isLoading == true) {
+              log("loading");
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is AuthControllerFeedbackState) {
+              log("success");
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                AwesomeDialog(
+                  context: context,
+                  dialogType: DialogType.success,
+                  animType: AnimType.rightSlide,
+                  title: 'success',
+                  desc: state.feedbackMessage,
+                  btnCancelOnPress: () {},
+                  btnOkOnPress: () {},
+                ).show();
+              });
+            } else if (state is AuthControllerFailureState) {
+              log("Failed");
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                AwesomeDialog(
+                  context: context,
+                  dialogType: DialogType.error,
+                  animType: AnimType.rightSlide,
+                  title: 'Error',
+                  desc: state.errorMessage,
+                  btnCancelOnPress: () {},
+                  btnOkOnPress: () {},
+                ).show();
+              });
+            }
+            AuthControllerBloc bloc = context.read<AuthControllerBloc>();
+            return ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                const GradientBackground(
                   children: [
-                    TextFormField(
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        controller: bloc.emailController,
-                        decoration: const InputDecoration(labelText: 'Email'),
-                        validator: Validator().validateMail),
-                    TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: bloc.passwordController,
-                      decoration: const InputDecoration(labelText: 'Password'),
-                      obscureText: true,
-                      validator: Validator().validatePass,
+                    Text(
+                      "Register",
+                      style: Consts.titleLarge,
                     ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (bloc.emailController.text.isNotEmpty &&
-                            bloc.passwordController.text.isNotEmpty) {
-                          bloc.add(SignUp(bloc.emailController.text.trim(),
-                              bloc.passwordController.text.trim()));
-                        }
-                      },
-                      child: const Text('Sign Up'),
-                    ),
+                    SizedBox(height: 6),
+                    Text("Register", style: Consts.bodySmall),
                   ],
                 ),
-              );
-            },
-          ),
+                const SizedBox(height: 20),
+                Form(
+                  key: bloc.formKey,
+                  child: Column(
+                    children: [
+                      AppTextFormField(
+                        controller: bloc.nameController,
+                        labelText: "Name",
+                        keyboardType: TextInputType.name,
+                        textInputAction: TextInputAction.next,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your Name';
+                          }
+                          return null;
+                        },
+                      ),
+                      AppTextFormField(
+                        controller: bloc.emailController,
+                        labelText: "Email",
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: Validator().validateMail,
+                      ),
+                      AppTextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        obscureText: true,
+                        controller: bloc.passwordController,
+                        labelText: "Password",
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.visiblePassword,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                      AppTextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        obscureText: true,
+                        controller: bloc.confirmPasswordController,
+                        labelText: "Confirm Password",
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.visiblePassword,
+                        validator: (value) {
+                          if (value != bloc.passwordController.text) {
+                            return 'Passwords does not match';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      FilledButton(
+                        onPressed: () async {
+                          if (bloc.emailController.text.isNotEmpty &&
+                              bloc.passwordController.text.isNotEmpty) {
+                            bloc.add(SignUp(
+                                bloc.emailController.text.trim(),
+                                bloc.passwordController.text.trim(),
+                                bloc.nameController.text.trim()));
+                          }
+                        },
+                        child: const Text('Sign Up'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
