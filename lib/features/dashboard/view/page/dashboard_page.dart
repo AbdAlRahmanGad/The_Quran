4d1c5f4d6f699583eea58ef52e45c71afc20df8d
Loay Ghreeb <loayahmed655@gmail.com>
2024-05-22 10:57:34 +0300

    Update dashboard_page.dart

--- lib/features/dashboard/view/page/dashboard_page.dart
+++ lib/features/dashboard/view/page/dashboard_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_quran/features/dashboard/controller/bloc/dashboard_bloc.dart';
-import 'package:the_quran/features/dashboard/view/page/ProfilePage.dart';
+import 'package:the_quran/features/dashboard/view/page/profile_page.dart';
 import 'package:the_quran/features/dashboard/view/page/GeneralAthkarPage.dart';
 import 'package:the_quran/features/dashboard/view/page/reciters_page.dart';
 import 'dart:convert';
